ad5ef86a33fb820e91b88f3705fef67157157c02
snasna <wisperinthedustbox@gmail.com>
2019-11-23 14:43:57 +0900

    fix anecdotes

--- part1/exercises/anecdotes/src/App.tsx
+++ part1/exercises/anecdotes/src/App.tsx
@@ -1,75 +1,33 @@
-import React, { useState } from "react";
+import React, { FC, useState } from "react";
 import "./App.css";
 
 const Button = ({ onClick, text }: { onClick: () => void; text: string }) => (
   <button onClick={onClick}>{text}</button>
 );
 
-const Statistic = ({ text, value }: { text: string; value: number }) => {
-  return (
-    <p>
-      {text}: {value}
-    </p>
-  );
-};
-const Statistics = ({
-  good,
-  bad,
-  neutral
-}: {
-  good: number;
-  bad: number;
-  neutral: number;
-}) => {
-  const all = good + neutral + bad;
-  const average = all > 0 ? (good * 1 + bad * -1) / all : 0;
-  const positive = all > 0 ? good / all : 0;
-  if (all === 0) {
-    return <h3>No feedback given</h3>;
-  }
-  return (
-    <>
-      <h3>statistics</h3>
-      <Statistic text="good" value={good} />
-      <Statistic text="neutral" value={neutral} />
-      <Statistic text="bad" value={bad} />
-      <Statistic text="all" value={all} />
-      <Statistic text="average" value={average} />
-      <Statistic text="positive" value={positive} />
-    </>
-  );
-};
-const App: React.FC = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const handleClickGood = () => {
-    setGood(good + 1);
+interface AppProps {
+  anecdotes: string[];
+}
+const App: FC<AppProps> = ({ anecdotes }) => {
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
+  const [mostVotes, setMostVotes] = useState(0);
+  const onClickNext = () =>
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  const onClickVote = (selected: number) => {
+    const newPoints = [...points];
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+    setMostVotes(newPoints.indexOf(Math.max(...newPoints)));
   };
-  const handleClickNeutral = () => {
-    setNeutral(neutral + 1);
-  };
-  const handleClickBad = () => {
-    setBad(bad + 1);
-  };
-
-  // const handleClick = (status: "good" | "bad" | "neutral") => {
-  //   // const setCountByStatus: { [key in typeof status]: () => void } = {
-  //   //   good: setGood(1),
-  //   //   bad: setBad,
-  //   //   neutral: setNeutral
-  //   // };
-  //   // setCountByStatus[status](1);
-  // };
   return (
-    <div className="App">
-      <section>
-        <h1>give feedback</h1>
-        <Button onClick={handleClickGood} text="good" />
-        <Button onClick={handleClickNeutral} text="neutral" />
-        <Button onClick={handleClickBad} text="bad" />
-      </section>
-      <Statistics good={good} bad={bad} neutral={neutral} />
+    <div>
+      <p>{anecdotes[selected]}</p>
+      <Button onClick={() => onClickVote(selected)} text="vote" />
+      <Button onClick={onClickNext} text="next anecdote" />
+      <h1>Anecdote with most vote</h1>
+      <p>{anecdotes[mostVotes]}</p>
+      <p>has {points[mostVotes]} votes</p>
     </div>
   );
 };
