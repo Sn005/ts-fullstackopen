572b437ce11192fee663f5967a624b80985040bf
snasna <wisperinthedustbox@gmail.com>
2019-11-23 14:01:56 +0900

    fix unicafe

--- part1/exercises/unicafe/src/App.tsx
+++ part1/exercises/unicafe/src/App.tsx
@@ -1,29 +1,75 @@
 import React, { useState } from "react";
-import Hello from "./Hello";
-import AppD from "./AppD";
 import "./App.css";
 
-const Display = ({ counter }: { counter: number }) => <div>{counter}</div>;
 const Button = ({ onClick, text }: { onClick: () => void; text: string }) => (
   <button onClick={onClick}>{text}</button>
 );
 
+const Statistic = ({ text, value }: { text: string; value: number }) => {
+  return (
+    <p>
+      {text}: {value}
+    </p>
+  );
+};
+const Statistics = ({
+  good,
+  bad,
+  neutral
+}: {
+  good: number;
+  bad: number;
+  neutral: number;
+}) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good * 1 + bad * -1) / all : 0;
+  const positive = all > 0 ? good / all : 0;
+  if (all === 0) {
+    return <h3>No feedback given</h3>;
+  }
+  return (
+    <>
+      <h3>statistics</h3>
+      <Statistic text="good" value={good} />
+      <Statistic text="neutral" value={neutral} />
+      <Statistic text="bad" value={bad} />
+      <Statistic text="all" value={all} />
+      <Statistic text="average" value={average} />
+      <Statistic text="positive" value={positive} />
+    </>
+  );
+};
 const App: React.FC = () => {
-  const name = "Peter";
-  const age = 10;
-  const [counter, setCounter] = useState(0);
-  // setTimeout(() => setCounter(counter + 1), 1000);
-  // const setToValue = (value: number) => setCounter(value);
-  const setToValue = (value: number) => () => setCounter(value);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const handleClickGood = () => {
+    setGood(good + 1);
+  };
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+  const handleClickBad = () => {
+    setBad(bad + 1);
+  };
 
+  // const handleClick = (status: "good" | "bad" | "neutral") => {
+  //   // const setCountByStatus: { [key in typeof status]: () => void } = {
+  //   //   good: setGood(1),
+  //   //   bad: setBad,
+  //   //   neutral: setNeutral
+  //   // };
+  //   // setCountByStatus[status](1);
+  // };
   return (
     <div className="App">
-      <Hello name="Maya" age={26 + 10} />
-      <Hello name={name} age={age} />
-      <Display counter={counter} />
-      <Button onClick={setToValue(counter + 1)} text="plus" />
-      <Button onClick={setToValue(0)} text="zero" />
-      <AppD />
+      <section>
+        <h1>give feedback</h1>
+        <Button onClick={handleClickGood} text="good" />
+        <Button onClick={handleClickNeutral} text="neutral" />
+        <Button onClick={handleClickBad} text="bad" />
+      </section>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
