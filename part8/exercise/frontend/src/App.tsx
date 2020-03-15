669f8688b950815716076c57ea9b377c85131113
Sn005 <living-inthe-junktown@hotmail.co.jp>
2020-03-15 20:15:19 +0900

    init: part8-exercise

---
+++ part8/exercise/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React, { FC, useState, useEffect } from "react";
+import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
+// import { ALL_PERSONS, FIND_PERSON, CREATE_PERSON, EDIT_NUMBER } from "./query";
+import "./App.css";
+function App() {
+  return <div>hoge</div>;
+}
+
+export default App;
