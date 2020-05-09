a83dec7a929ee0e175746aea58f49a52b8ce1136
Sn005 <living-inthe-junktown@hotmail.co.jp>
2020-05-09 15:12:19 +0900

    add: part8-ver2 tutorial client files

---
+++ part8-ver2/tutorial/client/src/App.tsx
@@ -0,0 +1,8 @@
+import React, { FC, useState, useEffect } from "react";
+import { useQuery, useLazyQuery, useMutation } from "@apollo/client";
+
+function App() {
+  return <>test</>;
+}
+
+export default App;
